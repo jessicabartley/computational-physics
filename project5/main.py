7bb87c35fa06c8741f53f656f1d91a99285e81da
George Song <george@55minutes.com>
2014-04-03 16:57:01 -0700

    Initial revision
    * Modeled each molecule as a object
    * Introduced `sweep()`

--- project5/main.py
+++ project5/main.py
@@ -9,70 +9,120 @@ from random import random
 BETA = 4
 EPSILON = 0.2
 NUMBER_OF_MOLECULES = 10
+NUMBER_OF_SWEEPS = 100000
 
 
-def create_initial_ensemble(no_molecules):
-    x = [0.5, 0.5, 0]   # space
-    p = [0, 0, 0]       # momenta
-    initial_ensemble = {}
-    initial_column = [x[0], x[1], x[2], p[0], p[1], p[2]]
-    for i in xrange(no_molecules):
-        make_column = {i : initial_column}
-        initial_ensemble.update(make_column)
-    return initial_ensemble
+class ThreeD(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def as_tuple(self):
+        return (self.x, self.y, self.z)
 
 
-def get_potential_energy(ensemble):
-    potential_energy = 0
-    for molecule in ensemble.itervalues():
-        potential_energy += molecule[2]
-    return potential_energy
+class Position(ThreeD):
+    pass
 
 
-def get_kinetic_energy(no_molecules, ensemble):
-    kinetic_energy = 0
-    for i in xrange(no_molecules):
-        kinetic_energy += 0.5 * (ensemble[i][3] ** 2 + ensemble[i][4] ** 2 + ensemble[i][5] ** 2) 
-    return kinetic_energy
+class Momentum(ThreeD):
+    pass
 
 
-def generate_trial_column(no_molecules, column_number):
-    ensemble = create_initial_ensemble(no_molecules)
-    for i in xrange(6):
-        ensemble[column_number][i] = ensemble[column_number][i] + (random() - 0.5) * EPSILON
-        if ensemble[column_number][i] > 1:
-            ensemble[column_number][i] = 1
-        if ensemble[column_number][i] < 0:
-            ensemble[column_number][i] = 0
-    trial_column = ensemble[column_number]
-    return trial_column
+class Molecule(object):
+    def __init__(self, position, momentum):
+        self.position = position
+        self.momentum = momentum
+
+    @property
+    def kinetic_energy(self):
+        return .5 * sum(map(lambda x: x ** 2, self.momentum.as_tuple()))
+
+    @property
+    def potential_energy(self):
+        return self.position.z
+
+    @property
+    def total_energy(self):
+        return self.kinetic_energy + self.potential_energy
 
 
-def compute_hamiltonian(no_molecules, ensemble):
-    hamiltonian = get_potential_energy(no_molecules, ensemble) + get_kinetic_energy(no_molecules, ensemble)
-    return hamiltonian
-
-def get_probability_distribution(no_molecules, ensemble):
-    probability_distribution = exp(-BETA * compute_hamiltonian(no_molecules, ensemble))
-    return probability_distribution
+def create_initial_ensemble(no_of_molecules):
+    initial_position = Position(0.5, 0.5, 0)
+    initial_momentum = Momentum(0, 0, 0)
+    return [Molecule(initial_position, initial_momentum)] * no_of_molecules
 
 
-def create_updated_ensemble(no_molecules, ensemble):
-    ensemble = create_initial_ensemble(no_molecules)
-    for i in xrange(no_molecules):
-        trial_column = generate_trial_column(no_molecules, i)
-        trial_ensemble = ensemble[i][trial_column]
-        if get_probability_distribution(no_molecules, trial_ensemble) >= get_probability_distribution(no_molecules, ensemble):
-            ensemble = trial_ensemble
-        elif get_probability_distribution(no_molecules, trial_ensemble) / get_probability_distribution(no_molecules, ensemble) >= random():
-            ensemble = trial_ensemble              
-    return ensemble
+def calculate_total_energy_for(ensemble):
+    total_energy = 0
+    for molecule in ensemble:
+        total_energy += molecule.total_energy
+    return total_energy
 
 
-ensemble = create_initial_ensemble(NUMBER_OF_MOLECULES)
-newcolumn = generate_trial_column(NUMBER_OF_MOLECULES, 0)
-potential = get_potential_energy(NUMBER_OF_MOLECULES, ensemble)
-kinetic = get_kinetic_energy(NUMBER_OF_MOLECULES, ensemble)
-H = compute_hamiltonian(NUMBER_OF_MOLECULES, ensemble)
-boltzman_dist = get_probability_distribution(NUMBER_OF_MOLECULES, ensemble)
-print(newcolumn, potential, kinetic, H, boltzman_dist)
+def randomly_vary_numbers(numbers):
+    # TODO: We need to take into consideration the boundaries
+    new_numbers = []
+    for n in numbers:
+        new_numbers.append(n + (random() - .5) * EPSILON)
+    return tuple(new_numbers)
+
+
+def create_trial_molecule(molecule):
+    new_position = Position(
+        *randomly_vary_numbers(molecule.position.as_tuple())
+    )
+    new_momentum = Momentum(
+        *randomly_vary_numbers(molecule.momentum.as_tuple())
+    )
+    return Molecule(new_position, new_momentum)
+
+
+def has_greater_energy(trial_molecule, molecule):
+    return trial_molecule.total_energy >= molecule.total_energy
+
+
+def get_probability_distribution(ensemble):
+    return exp(-BETA * calculate_total_energy_for(ensemble))
+
+
+def roll_the_dice(ensemble, trial_molecule, molecule):
+    index = ensemble.index(molecule)
+    trial_ensemble = list(ensemble)  # Copy the original ensemble
+    trial_ensemble[index] = trial_molecule
+    current_p = get_probability_distribution(ensemble)
+    trial_p = get_probability_distribution(trial_ensemble)
+    if current_p:
+        return trial_p / current_p >= random()
+    else:
+        return False
+
+
+def keep_or_replace_molecule(ensemble, molecule):
+    trial_molecule = create_trial_molecule(molecule)
+    if has_greater_energy(trial_molecule, molecule):
+        return trial_molecule
+    if roll_the_dice(ensemble, trial_molecule, molecule):
+        return trial_molecule
+    return molecule
+
+
+def sweep(ensemble):
+    new_ensemble = []
+    for molecule in ensemble:
+        new_ensemble.append(keep_or_replace_molecule(ensemble, molecule))
+    return new_ensemble
+
+
+def main():
+    ensemble = create_initial_ensemble(NUMBER_OF_MOLECULES)
+    system_energies = [calculate_total_energy_for(ensemble)]
+    for i in xrange(NUMBER_OF_SWEEPS):
+        ensemble = sweep(ensemble)
+        system_energies.append(calculate_total_energy_for(ensemble))
+    print(system_energies)
+
+
+if __name__ == '__main__':
+    main()
