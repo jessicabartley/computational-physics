106df552bd54f2c70fb0a2ae3535bea883db3b69
Jessica Bartley <jessica.e.bartley@gmail.com>
2014-04-03 12:22:58 -0400

    Some minor edits to the project description text and the stat of a code for project 5

---
+++ project5/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import division
+
+BOLTZMANNS_CONST = 1.38 * 10 ** -23 # units (m^2 * kg) / (s^2 * K)
+RESEVOIR_TEMPERATURE = 293 # units K
+NUMBER_OF_MOLECULES = 10
+
+def setup_initial_ensemble(no_molecules):
+    
+    return initial_ensemble
