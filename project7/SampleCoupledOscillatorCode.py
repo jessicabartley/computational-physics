255267eb24cf76c8031182d575261e9fb1e8ae7b
George Song <george@55minutes.com>
2014-04-22 00:27:57 -0400

    PEP8

--- project7/SampleCoupledOscillatorCode.py
+++ project7/SampleCoupledOscillatorCode.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pylab import *
+from pylab import array, plot, show, xlabel, ylabel, zeros
 from numpy import linspace, cos, sin
 from scipy.integrate import odeint
 
@@ -25,34 +25,20 @@ k = 3.5
 m = 0.2
 gravity = 9.8
 
+
 def spring_pendulum(y, time):
     g0 = y[1]
     g1 = (L_o + y[0]) * y[3] * y[3] - k / m * y[0] + gravity * cos(y[2])
     g2 = y[3]
-    g3 = -(gravity * sin(y[2]) + 2.0 * y[1] * y[3]) / (L_o +y[0])
+    g3 = -(gravity * sin(y[2]) + 2.0 * y[1] * y[3]) / (L_o + y[0])
     return array([g0, g1, g2, g3])
 
 answer = odeint(spring_pendulum, y, time)
 
-xdata = (L_o + answer[:,0]) * sin(answer[:,2])
-ydata = -(L_o + answer[:,0]) * cos(answer[:,2])
+xdata = (L_o + answer[:, 0]) * sin(answer[:, 2])
+ydata = -(L_o + answer[:, 0]) * cos(answer[:, 2])
 
 plot(xdata, ydata, 'r-')
 xlabel("Horizontal Position")
 ylabel("Vertical Position")
 show()
-
-#    return (g0, g1, g2, g3)
-
-#def main():
-#    answer = odeint(spring_pendulum, y, time)
-#    for row in xrange(NUMBER_OF_STEPS):
-#        print answer[row, 0], answer[row, 1], answer[row, 2], answer[row, 3]
-
-#if __name__ == '__main__':
-#    main()
-
-
-
-
-
